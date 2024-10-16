e2053a86965df042f64983f3ac75d95ae94af40f
Sergei Mikurov <454004@mail.ru>
2024-10-16 06:16:10 +0500

    Update constants

--- src/wse/constants.py
+++ src/wse/constants.py
@@ -7,16 +7,19 @@ from http import HTTPStatus
 ########################################################################
 
 FOREIGN_BOX = 'foreign_box'
+FOREIGN_CREATE_BOX = 'foreign_create_box'
 FOREIGN_EXERCISE_BOX = 'foreign_exercise_box'
+FOREIGN_LIST_BOX = 'foreign_list_box'
 FOREIGN_PARAMS_BOX = 'foreign_params_box'
+FOREIGN_UPDATE_BOX = 'foreign_update_box'
 GLOSSARY_BOX = 'glossary_box'
 GLOSSARY_EXERCISE_BOX = 'glossary_exercise_box'
 GLOSSARY_PARAMS_BOX = 'glossary_exercise_params_box'
 LOGIN_BOX = 'login_box'
 MAIN_BOX = 'main_box'
 USER_BOX = 'user_box'
-USER_UPDATE_BOX = 'user_update_box'
 USER_CREATE_BOX = 'user_create_box'
+USER_UPDATE_BOX = 'user_update_box'
 
 ########################################################################
 # Url and statuses
@@ -37,6 +40,9 @@ FOREIGN_PROGRESS_PATH = '/api/v1/foreign/progress/'
 """
 FOREIGN_PARAMS_PATH = '/api/v1/foreign/params/'
 """Learning foreign word exercise parameters path (`str`).
+"""
+FOREIGN_LIST_CREATE_PATH = '/api/v1/foreign/'
+"""Add foreign word to dictionary (`str`).
 """
 GLOSSARY_EXERCISE_PATH = '/api/v1/glossary/exercise/'
 """Glossary exercise path (`str`).
@@ -75,7 +81,7 @@ DETAIL = 'detail'
 EDGE_PERIODS = 'edge_period_items'
 ERROR = 'error'
 EXERCISE_CHOICES = 'exercise_choices'
-STYLE = 'style'
+FOREIGN_WORD = 'foreign_word'
 HUMANLY = 'humanly'
 ID = 'id'
 KNOW = 'know'
@@ -86,9 +92,22 @@ NOT_KNOW = 'not_know'
 PASSWORD = 'password'
 PERIOD_END = 'period_end_date'
 PERIOD_START = 'period_start_date'
+PREVIOUS = 'previous'
 PROGRESS = 'progress'
 QUESTION = 'question'
 QUESTION_TEXT = 'question_text'
+RESULTS = 'results'
+RUSSIAN_WORD = 'russian_word'
+STYLE = 'style'
 TERM_ID = 'term_id'
 TIMEOUT = 'timeout'
 USERNAME = 'username'
+
+########################################################################
+# Application settings
+########################################################################
+
+FONT_SIZE_APP = 11
+BUTTON_HEIGHT = 60
+INPUT_HEIGHT = 60
+TITLE_LABEL_HEIGHT = 35
