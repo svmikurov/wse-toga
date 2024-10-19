955172bc8abc3945c4165e87297795c8a916525d
Sergei Mikurov <454004@mail.ru>
2024-10-20 02:17:21 +0500

    Add validate credentials

---
+++ src/wse/contrib/validator.py
@@ -0,0 +1,75 @@
+"""Validators."""
+
+CYRILLIC_LOWER = ''.join(chr(i) for i in range(1072, 1104))
+CYRILLIC_UPPER = ''.join(chr(i) for i in range(1040, 1072))
+LATIN_LOWER = ''.join(chr(i) for i in range(65, 90))
+LATIN_UPPER = ''.join(chr(i) for i in range(97, 122))
+NUMBERS = '0123456789'
+SYMBOLS = '@.+-_'
+
+MAX_USER_LENGTH = 150
+MIN_PASSWORD_LENGTH = 8
+
+SYMBOL_ERROR = 'Допустимы только буквы, цифры и символы @/./+/-/_.'
+LENGTH_ERROR = 'Длина имени не должна превышать 150 символов.'
+PASSWORD_LENGTH_ERROR = 'Пароль должен содержать как минимум 8 символов'
+
+
+def validate_username(username: str) -> list:
+    """Validate the username.
+
+    >>> validate_username('username')
+    []
+    >>> validate_username('wrong!')
+    ['Допустимы только буквы, цифры и символы @/./+/-/_.']
+    >>> validate_username('@.+-_1иG')
+    []
+    """
+    allowed = (
+        CYRILLIC_LOWER
+        + CYRILLIC_UPPER
+        + LATIN_LOWER
+        + LATIN_UPPER
+        + NUMBERS
+        + SYMBOLS
+    )
+
+    errors = []
+    for liter in username:
+        if liter not in allowed:
+            errors.append(SYMBOL_ERROR)
+            break
+
+    if len(username) > MAX_USER_LENGTH:
+        errors.append(LENGTH_ERROR)
+    return errors
+
+
+def validate_password(password: str) -> list:
+    """Validate the user password.
+
+    >>> validate_password('12345678')
+    ['Пароль не может состоять только из цифр']
+    >>> validate_password('xxx')
+    ['Пароль должен содержать как минимум 8 символов']
+    >>> validate_password('password')
+    []
+    """
+    errors = ['Пароль не может состоять только из цифр']
+    for item in password:
+        if item not in NUMBERS:
+            errors.pop()
+            break
+
+    if len(password) < MIN_PASSWORD_LENGTH:
+        errors.append(PASSWORD_LENGTH_ERROR)
+
+    return errors
+
+
+def validate_credentials(credentials: dict) -> list:
+    """Validate the user credentials."""
+    errors = []
+    errors.extend(validate_username(credentials['username']))
+    errors.extend(validate_password(credentials['password']))
+    return errors
--- tests/test_validator.py
+++ tests/test_validator.py
@@ -2,12 +2,16 @@
 
 import pytest
 
-from wse.contrib.utils import validate_username, validate_password
+from wse.contrib.validator import validate_username, validate_password
 
 
 @pytest.mark.parametrize(
     "test_input,expected",
-    [('username', True), ('wrong!', False)]
+    [
+        ('username', []),
+        ('wrong!', ['Допустимы только буквы, цифры и символы @/./+/-/_.']),
+        ('@.+-_1иG', []),
+    ]
 )
 def test_username_validator(test_input, expected):
     assert validate_username(test_input) == expected
@@ -15,7 +19,11 @@ def test_username_validator(test_input, expected):
 
 @pytest.mark.parametrize(
     "test_input,expected",
-    [('password', True), ('passwor', False), ('00000000', False)]
+    [
+        ('12345678', ['Пароль не может состоять только из цифр']),
+        ('passwor', ['Пароль должен содержать как минимум 8 символов']),
+        ('password', []),
+    ]
 )
 def test_password_validator(test_input, expected):
     assert validate_password(test_input) == expected
