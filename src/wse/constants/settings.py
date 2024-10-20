96f058bc3eeda56fc3ad88c68cb4e37bd6223816
Sergei Mikurov <454004@mail.ru>
2024-10-20 11:25:26 +0500

    Add titles and lable style

--- src/wse/constants/settings.py
+++ src/wse/constants/settings.py
@@ -1,4 +1,5 @@
 """The application widget settings constants."""
+from travertino.constants import ITALIC
 
 DEFAULT_TIMEOUT = 5
 """Time to answer on task question in exercises (`int`).
@@ -15,4 +16,36 @@ PADDING_AROUND_WIDGET = (PADDING_SM, PADDING_SM, PADDING_SM, PADDING_SM)
 FONT_SIZE_APP = 11
 BUTTON_HEIGHT = 60
 INPUT_HEIGHT = 60
+
+#########################################################################
+# TextDisplay
+TEXT_DISPLAY_FONT_SIZE = 14
+TEXT_DISPLAY_FONT_STYLE = ITALIC
+TEXT_DISPLAY_PADDING = (2, 2, 2, 2)
+
+#########################################################################
+# Titles
+
+TITLE_LABEL_FONT_SIZE = 16
 TITLE_LABEL_HEIGHT = 35
+TITLE_LABEL_PADDING = (5, 0, 10, 0)
+# Main
+TITLE_MAIN = 'WSELFEDU'
+# User
+TITLE_USER_MAIN = 'Учетная запись'
+TITLE_USER_UPDATE = 'Изменить имя'
+TITLE_USER_AUTH = 'Вход в учетную запись'
+# Glossary
+TITLE_GLOSSARY_MAIN = 'Глоссарий'
+TITLE_GLOSSARY_CREATE = 'Добавить термин'
+TITLE_GLOSSARY_UPDATE = 'Изменить термин'
+TITLE_GLOSSARY_LIST = 'Список терминов'
+TITLE_GLOSSARY_PARAMS = 'Параметры изучения терминов'
+TITLE_GLOSSARY_EXERCISE = 'Изучение терминов'
+# Foreign
+TITLE_FOREIGN_MAIN = 'Иностранный словарь'
+TITLE_FOREIGN_CREATE = 'Добавить термин'
+TITLE_FOREIGN_UPDATE = 'Изменить термин'
+TITLE_FOREIGN_LIST = 'Список иностранных слов'
+TITLE_FOREIGN_PARAMS = 'Параметры изучения слов'
+TITLE_FOREIGN_EXERCISE = 'Изучение слов'
