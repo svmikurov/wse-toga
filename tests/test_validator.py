b9d24d64a581b4fa8781a60764ad49a90b6d6e4b
Sergei Mikurov <454004@mail.ru>
2024-10-31 23:21:29 +0500

    Add test main page box

--- tests/test_validator.py
+++ tests/test_validator.py
@@ -2,28 +2,30 @@
 
 import pytest
 
-from wse.contrib.validator import validate_username, validate_password
+from wse.contrib.validator import validate_password, validate_username
 
 
 @pytest.mark.parametrize(
-    "test_input,expected",
+    'test_input,expected',
     [
         ('username', []),
         ('wrong!', ['Допустимы только буквы, цифры и символы @/./+/-/_.']),
         ('@.+-_1иG', []),
-    ]
+    ],
 )
-def test_username_validator(test_input, expected):
+def test_username_validator(test_input: str, expected: str) -> None:
+    """Assert that the username is valid."""
     assert validate_username(test_input) == expected
 
 
 @pytest.mark.parametrize(
-    "test_input,expected",
+    'test_input, expected',
     [
         ('12345678', ['Пароль не может состоять только из цифр']),
         ('passwor', ['Пароль должен содержать как минимум 8 символов']),
         ('password', []),
-    ]
+    ],
 )
-def test_password_validator(test_input, expected):
+def test_password_validator(test_input: str, expected: str) -> None:
+    """Assert that the password is valid."""
     assert validate_password(test_input) == expected
