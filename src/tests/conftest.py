8744fadbf7021a2bcf7f41333cbff1f15640869d
Sergei Mikurov <454004@mail.ru>
2024-11-04 16:51:44 +0500

    Add selection_params fixture

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -4,7 +4,11 @@ import pytest
 
 import toga
 
+from tests.utils import FixtureReader
 from wse.app import WSE
+
+FIXTURE = 'params_glossary.json'
+PARAMS = FixtureReader(FIXTURE).json()
 
 
 @pytest.fixture(scope='session')
@@ -23,3 +27,30 @@ def wse(event_loop):
         pass
 
     return WSE(formal_name="Test App", app_id="org.beeware.toga.test-app")
+
+
+@pytest.fixture(scope='function')
+def selection_params(wse: WSE) -> None:
+    """Populate exercise params selections form fixture."""
+    wse.box_glossary_params.selection_start_period.set_items(
+        PARAMS['exercise_choices']['edge_period_items'],
+        PARAMS['lookup_conditions']['period_start_date'],
+    )
+    wse.box_glossary_params.selection_end_period.set_items(
+        PARAMS['exercise_choices']['edge_period_items'],
+        PARAMS['lookup_conditions']['period_end_date'],
+    )
+    wse.box_glossary_params.selection_category.set_items(
+        PARAMS['exercise_choices']['categories'],
+        PARAMS['lookup_conditions']['category'],
+    )
+    wse.box_glossary_params.selection_progress.set_items(
+        PARAMS['exercise_choices']['progress'],
+        PARAMS['lookup_conditions']['progress'],
+    )
+    wse.box_glossary_params.input_count_first.value = (
+        PARAMS['lookup_conditions']['count_first']
+    )
+    wse.box_glossary_params.input_count_last.value = (
+        PARAMS['lookup_conditions']['count_last']
+    )
