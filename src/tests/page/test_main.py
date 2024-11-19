27046bbab610eec64776f85f66e312da61d5b03a
Sergei Mikurov <454004@mail.ru>
2024-11-19 10:10:12 +0500

    Add tests of goto exrcise buttons at main page

--- src/tests/page/test_main.py
+++ src/tests/page/test_main.py
@@ -5,10 +5,14 @@ Testing:
  * Changing window contents when pressing move buttons.
  * Control the widget count for test.
 """
+from unittest.mock import patch, MagicMock, AsyncMock
+
+from toga.handlers import simple_handler
 
 from tests.utils import run_until_complete
 from wse.app import WSE
 from wse.constants import HOST_API
+from wse.general.goto_handler import goto_foreign_exercise
 
 WIDGET_COUNT = 8
 """Widget count at testing box container (int).
@@ -40,7 +44,7 @@ def test_btn_login(wse: WSE) -> None:
     assert wse.main_window.content == wse.box_login
 
 
-def test_click_goto_foreign_btn(wse: WSE) -> None:
+def test_btn_goto_foreign_main(wse: WSE) -> None:
     """Test click on button to go to foreign page box."""
     btn = wse.box_main.btn_goto_foreign_main
 
@@ -53,7 +57,7 @@ def test_click_goto_foreign_btn(wse: WSE) -> None:
     assert wse.main_window.content == wse.box_foreign_main
 
 
-def test_click_goto_glossary_btn(wse: WSE) -> None:
+def test_btn_goto_glossary_main(wse: WSE) -> None:
     """Test click on button to go to glossary page box."""
     btn = wse.box_main.btn_goto_glossary_main
 
@@ -70,3 +74,37 @@ def test_info_panel(wse: WSE) -> None:
     """Test info panel placeholder."""
     info_panel = wse.box_main.info_panel
     assert info_panel.placeholder == f'Ready for connect to {HOST_API}'
+
+
+def test_btn_goto_foreign_exercise(wse: WSE) -> None:
+    """Test quik start button of foreign exercise."""
+    btn = wse.box_main.btn_goto_foreign_exercise
+
+    # Button has specific text.
+    assert btn.text == 'Изучение слов'
+
+    # Invoke the callback.
+    btn._impl.simulate_press()
+
+    # Run a fake main loop.
+    run_until_complete(wse)
+
+    # Window switch.
+    assert wse.main_window.content == wse.box_foreign_exercise
+
+
+def test_btn_goto_glossary_exercise(wse: WSE) -> None:
+    """Test quik start button of glossary exercise."""
+    btn = wse.box_main.btn_goto_glossary_exercise
+
+    # Button has specific text.
+    assert btn.text == 'Изучение терминов'
+
+    # Invoke the callback.
+    btn._impl.simulate_press()
+
+    # Run a fake main loop.
+    run_until_complete(wse)
+
+    # Window switch.
+    assert wse.main_window.content == wse.box_glossary_exercise
